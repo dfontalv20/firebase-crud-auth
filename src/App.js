c7856fa76cdff77f606a671a68627149bb6f4407
diegofontalvoDT <diego.fontalvo@devitech.com.co>
2022-06-03 02:30:23 -0500

    Crud firebase servicios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { createContext, useEffect, useState } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import { auth } from './config/firebase';
+
+export const UserContext = createContext(null);
 
 function App() {
+  const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user)
+        navigate('/dashboard')
+        return setLoading(false)
+      }
+      setUser({})
+      navigate('/')
+      setLoading(false)
+    })
+  }, [])
+
+  useEffect(() => {
+    if(!user) setLoading(true)
+  }, [user])
+  
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      { loading ? <div className='d-flex w-100 vh-100'><span className='spinner-border m-auto'/></div> :
+        <UserContext.Provider value={user} >
+          <Routes>
+            <Route path='' element={<Login />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+          </Routes>
+        </UserContext.Provider >}
+    </>
   );
 }
 
